7e109ab661f3e1116ec738be99bff147cebe91c4
vijay-kartik <kartik.iit96@gmail.com>
2021-12-19 19:24:43 +0530

    first commit

---
+++ src/com/greatlearning/main/Q2_CurrencyPayment.java
@@ -0,0 +1,27 @@
+package com.greatlearning.main;
+
+import java.util.Scanner;
+
+import com.greatlearning.service.*;
+
+public class Q2_CurrencyPayment {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of curency denominations");
+		int sizeOfDenominations = sc.nextInt();
+		
+		
+		System.out.println("Enter the currency denominations value");
+		int denominations[] = new int[sizeOfDenominations];
+		for(int i = 0; i < sizeOfDenominations; i++) {
+			denominations[i] = sc.nextInt();
+		}
+		System.out.println("Enter the amount you want to pay");
+		int amount = sc.nextInt();
+		MergeSort mergeSort = new MergeSort();
+		mergeSort.applySortDesc(denominations, 0, sizeOfDenominations - 1);
+		NotesCount notesCount = new NotesCount();
+		notesCount.notesCountImpl(denominations, amount);
+		
+	}
+}
